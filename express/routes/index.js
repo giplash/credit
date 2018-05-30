067d7733f648eeeb1f347401c789ffdcff76d22f
giplash <anashkinviktar@gmail.com>
2018-05-30 19:30:19 +0300

    add first layout version

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const data = require('./data')
+
+const march = data[0];
+const april = data[1];
+const may = data[2];
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', {data: may});
+});
+
+router.get('/may', (req, res) => {
+  res.render('index', {data: may});
+});
+
+router.get('/april', (req, res) => {
+  res.render('index', {data: april});
+});
+
+router.get('/march', (req, res) => {
+  res.render('index', {data: march});
 });
 
 module.exports = router;
