9884b2819d4cbd43463e9b16d738ae2bdc401e78
vmikulich <vladm1k453@gmail.com>
2018-05-31 14:12:34 +0300

    change likes

--- express/public/js/script.js
+++ express/public/js/script.js
@@ -4,8 +4,8 @@ const likes = document.querySelectorAll('.like');
       if (!(this.getAttribute('clicked') == 'true')) {
         this.setAttribute('src', '/img/redHeart.png');
         this.setAttribute('clicked', 'true');
-        this.style.top = '2px';
-        this.style.width = '43px';
+        this.style.top = '5px';
+        this.style.width = '40px';
         this.nextSibling.nextSibling.innerHTML = +this.nextSibling.nextSibling.innerHTML + 1
       } else {
         this.setAttribute('src', '/img/blackHeart.png');
